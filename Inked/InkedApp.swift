7553da2b5a8b8848f7fe3cf6d96c3fda4624ff4a
korabelnikov <nkorabelnikov22@gmail.com>
2024-02-28 21:05:24 +1000

    Initial Commit

---
+++ Inked/InkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkedApp.swift
+//  Inked
+//
+//  Created by Никита on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct InkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
